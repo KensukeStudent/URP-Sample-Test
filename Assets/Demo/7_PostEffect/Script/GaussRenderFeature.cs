a55440594a19b5801ca170029907093713eaf95b
nagase <nagaken8937@gmail.com>
2025-12-29 23:52:53 +0900

    ガウスブラーXY

--- Assets/Demo/7_PostEffect/Script/GaussRenderFeature.cs
+++ Assets/Demo/7_PostEffect/Script/GaussRenderFeature.cs
@@ -114,6 +114,10 @@ public class GaussRenderFeature : ScriptableRendererFeature
             // CameraData
             UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
 
+            // ------------------------------------------------------------ 
+            // camera -> gaussX texture
+            // ------------------------------------------------------------
+
             // 横サイズは半分のテクスチャー: ダウンサンプリング
             // 16:9 -> 8:9
             RenderTextureDescriptor desc = cameraData.cameraTargetDescriptor;
@@ -151,18 +155,56 @@ public class GaussRenderFeature : ScriptableRendererFeature
                 });
             }
 
-            // gaussXTexture RT -> camera color RT
+            // ------------------------------------------------------------ 
+            // gaussX texture -> gaussY texture
+            // ------------------------------------------------------------
+
+            // 横サイズは半分のテクスチャー: ダウンサンプリング
+            // 8:9 -> 8:4.5
+            desc = cameraData.cameraTargetDescriptor;
+            desc.colorFormat = RenderTextureFormat.ARGB32; // Enable alpha
+            desc.msaaSamples = 1;
+            desc.depthBufferBits = 0;
+            desc.height = Screen.height / 2;
+            desc.width = Screen.width / 2;
+
+            // Y方向ブラー用テクスチャ作成
+            TextureHandle gaussYTextureHandle = UniversalRenderer.CreateRenderGraphTexture(renderGraph, desc, "_GaussYTexture", true, FilterMode.Bilinear);
+
+            // gaussXTexture RT -> gaussYTexture RT
+            using (IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass(passName, out PassData passData, this.profilingSampler))
+            {
+                builder.UseTexture(gaussXTextureHandle, AccessFlags.Read);
+
+                builder.SetRenderAttachment(gaussYTextureHandle, 0, AccessFlags.Write);
+
+                // Resources/References for pass execution
+                // Blit source texture
+                passData.sourceTextureHandle = gaussXTextureHandle;
+                // Blit material
+                passData.material = material;
+
+                // Set render function
+                // Y方向ブラー: 1パス目を使用
+                builder.SetRenderFunc((PassData passData, RasterGraphContext graphContext) =>
+                {
+                    RasterCommandBuffer cmd = graphContext.cmd;
+
+                    Blitter.BlitTexture(cmd, passData.sourceTextureHandle, new Vector4(1, 1, 0, 0), passData.material, 1);
+                });
+            }
+
+            // gaussYTexture RT -> camera color RT
             using (IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass(passName, out PassData passData, this.profilingSampler))
             {
                 // Set tempRT for read
-                builder.UseTexture(gaussXTextureHandle, AccessFlags.Read);
-
+                builder.UseTexture(gaussYTextureHandle, AccessFlags.Read);
                 // Set camera color RT for write
                 builder.SetRenderAttachment(cameraColorTextureHandler, 0, AccessFlags.Write);
 
                 // Resources/References for pass execution
                 // Blit source texture
-                passData.sourceTextureHandle = gaussXTextureHandle;
+                passData.sourceTextureHandle = gaussYTextureHandle;
                 passData.material = null;
 
                 builder.SetRenderFunc((PassData passData, RasterGraphContext graphContext) =>
@@ -173,67 +215,4 @@ public class GaussRenderFeature : ScriptableRendererFeature
             }
         }
     }
-
-    // ------------------------------------------------------------------
-    // CombinePass
-    // ------------------------------------------------------------------
-
-    // public class CombinePass : ScriptableRenderPass
-    // {
-    //     private Material _material;
-
-    //     private class PassData
-    //     {
-    //         public Material Material;
-    //         public TextureHandle sourceTextureHandle;
-    //     }
-
-    //     public CombinePass(RenderPassEvent renderPassEvent, Material material)
-    //     {
-    //         this.renderPassEvent = renderPassEvent;
-    //         this.profilingSampler = new ProfilingSampler(nameof(CombinePass));
-    //         _material = material;
-    //     }
-
-    //     public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
-    //     {
-    //         var resourceData = frameData.Get<UniversalResourceData>();
-    //         var sourceTextureHandle = resourceData.activeColorTexture;
-
-    //         // 現在アクティブのカメラカラーを元に、合成テクスチャを作成
-    //         var targetDesc = renderGraph.GetTextureDesc(sourceTextureHandle);
-    //         targetDesc.name = "_CombineTexture";
-    //         targetDesc.clearBuffer = false;
-    //         targetDesc.depthBufferBits = 0;
-    //         var combineTextureHandle = renderGraph.CreateTexture(targetDesc);
-
-    //         // 合成したテクスチャをカメラカラーに設定
-    //         resourceData.cameraColor = combineTextureHandle;
-
-    //         // カメラカラーを反転し、出力用のテクスチャに描画するRasterRenderPassを作成し、RenderGraphに追加
-    //         using (var builder = renderGraph.AddRasterRenderPass<PassData>("CombinePass", out var passData))
-    //         {
-    //             // 描画ターゲット設定
-    //             builder.SetRenderAttachment(combineTextureHandle, 0, AccessFlags.Write);
-    //             builder.UseTexture(sourceTextureHandle, AccessFlags.Read);
-
-    //             // GlobalTextureの使用宣言
-    //             builder.UseGlobalTexture(Shader.PropertyToID("_NormalEdgeTexture"), AccessFlags.Read);
-
-    //             // passDataに必要なデータを入れる
-    //             passData.Material = _material;
-    //             passData.sourceTextureHandle = sourceTextureHandle;
-
-    //             // すべてのGlobalTextureを使用申請
-    //             // builder.UseAllGlobalTexture(true);
-    //             builder.SetRenderFunc(static (PassData data, RasterGraphContext context) => ExecutePass(data, context));
-    //         }
-    //     }
-
-    //     private static void ExecutePass(PassData passData, RasterGraphContext graphContext)
-    //     {
-    //         RasterCommandBuffer cmd = graphContext.cmd;
-    //         Blitter.BlitTexture(cmd, passData.sourceTextureHandle, new Vector4(1, 1, 0, 0), passData.Material, 1);
-    //     }
-    // }
 }
