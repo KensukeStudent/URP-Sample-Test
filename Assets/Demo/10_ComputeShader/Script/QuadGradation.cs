da706629ce09de5698e4392f8da8194e28568966
nagase <nagaken8937@gmail.com>
2026-02-07 13:01:28 +0900

    パラメータで変更できるように

--- Assets/Demo/10_ComputeShader/Script/QuadGradation.cs
+++ Assets/Demo/10_ComputeShader/Script/QuadGradation.cs
@@ -8,6 +8,9 @@ public class QuadGradation : MonoBehaviour
     [SerializeField]
     private RenderTexture renderTexture; // 256x256 = 65536ピクセル
 
+    [SerializeField]
+    private int lineWidth = 20;
+
     private void Start()
     {
         var material = GetComponent<Renderer>().material;
@@ -15,6 +18,13 @@ public class QuadGradation : MonoBehaviour
 
         var kernelIndex = computeShader.FindKernel("CSMain");
         computeShader.SetTexture(kernelIndex, "Result", renderTexture);
+    }
+
+    private void Update()
+    {
+        computeShader.SetInt("LineWidth", lineWidth);
+
+        var kernelIndex = computeShader.FindKernel("CSMain");
         computeShader.Dispatch(kernelIndex, renderTexture.width / 8, renderTexture.height / 8, 1);
     }
 
